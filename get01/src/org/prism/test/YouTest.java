ec028db5a6c64879fb315e4934afae2c5c385832
wpl <2928673313@qq.com>
2021-06-29 17:59:45 +0800

    第三次提交   copy了一份

--- get01/src/org/prism/test/YouTest.java
+++ get01/src/org/prism/test/YouTest.java
@@ -1,6 +1,6 @@
 package org.prism.test;
 
-public class MyTest {
+public class YouTest {
     public static void main(String[] args) {
         System.out.println("MyTest.main");
         System.out.println("MyTest.main");
